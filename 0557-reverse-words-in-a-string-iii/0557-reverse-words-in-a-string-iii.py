217a5ac22798ee1ee1b8ada329fdf103877865ec
Luiz Filipe B Miranda <contatoluizfbm@gmail.com>
2026-01-23 12:25:14 -0300

    0557-reverse-words-in-a-string-iii 01-23-2026 .py

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ''
+        l, r = 0, 0 
+        while r < len(s):
+            if s[r] != ' ':
+                r += 1
+            else:
+                res += s[l:r+1][::-1]
+                r += 1
+                l = r
+        res += ' '
+        res += s[l:r+2][::-1]
+        return res[1:]
